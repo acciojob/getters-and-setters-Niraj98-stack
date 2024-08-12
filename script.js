8dc967e496c588027288e31d0043fc596b540217
Niraj chaudhari <151720495+Niraj98-stack@users.noreply.github.com>
2024-08-12 20:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 //complete this code
-class Person {}
+// Define the Person class
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+	}
+    get name() {
+        return this._name;
+    }
 
-class Student extends Person {}
 
-class Teacher extends Person {}
+    set age(age) {
+        this._age = age;
+    }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+
+    get age() {
+        return this._age;
+    }
+}
+
+
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
+
+// Example usage
+const person = new Person("John", 25);
+console.log(person.name); 
+
+person.age = 30;
+console.log(person.age); 
+
+const student = new Student("Alice", 22);
+student.study(); 
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach();
